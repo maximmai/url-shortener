7ed0f63aba543623a545c7a47e7bb6202da55699
Maxim Mai <maxim.sl.mai@gmail.com>
2025-08-08 09:45:47 -0400

    use local sqlite instead of redis, for demo purpose

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const bodyParser = require('body-parser');
-const RedisClient = require('./redis');
+const DBClient = require('./db');
 const {hash, sanitize, validateHash, rateLimiter} = require('./lib');
 
-const APP_PORT = process.env.APP_PORT;
-const BASE_URL = process.env.BASE_URL;
+const APP_PORT = process.env.APP_PORT || 3000;
+const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
 const SUPPORTED_PROTOCOLS = ["https", "http"];
 
 const main = async () => {
@@ -17,15 +17,15 @@ const main = async () => {
 
     app.use(rateLimiter({
         tokensPerInterval: 1,
-        interval: 2000,        // Add 1 token every 2 seconds
-        bucketSize: 5          // Max burst of 5
+        interval: 2000,
+        bucketSize: 5
     }));
 
     app.get('/:hashed', async (req, res) => {
         if (!validateHash(req.params.hashed)) {
             return res.status(404).send("invalid key");
         }
-        const result = await RedisClient.get(req.params.hashed);
+        const result = await DBClient.get(req.params.hashed);
         const response = result ? result : null;
         const sanitized = sanitize(response, SUPPORTED_PROTOCOLS);
         if (sanitized) {
@@ -33,30 +33,28 @@ const main = async () => {
         } else {
             res.status(404).send(null);
         }
-
     });
 
-    app.post('/url/shorten', (req, res) => {
+    app.post('/url/shorten', async (req, res) => {
         const originalUrl = req.body.url;
         const hashed = hash(originalUrl);
         const sanitized = sanitize(originalUrl, SUPPORTED_PROTOCOLS);
         if (sanitized) {
-            RedisClient.set(hashed, originalUrl);
+            await DBClient.set(hashed, originalUrl);
             res.send({
                 url: `${BASE_URL}/${hashed}`,
             });
-        } else{
+        } else {
             res.status(400).send(null);
         }
-
     });
 
-    app.get('/ping', (req, res) => {
+    app.get('/test/ping', (req, res) => {
         res.send('Request accepted!');
     });
 
     app.listen(APP_PORT, () => {
-        console.log(` url-shortener app listening at http://localhost:${APP_PORT}`)
+        console.log(`url-shortener app listening at http://localhost:${APP_PORT}`)
     });
 };
 
@@ -64,7 +62,6 @@ const main = async () => {
     try {
         await main();
     } catch (e) {
-        // Deal with the fact the chain failed
         console.error(e);
     }
 })();
